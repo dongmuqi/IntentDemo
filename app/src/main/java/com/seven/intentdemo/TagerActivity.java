13322153f1d8f04c1f0ec08f802c01ab05e99f97
董牧齐 <sevenluck777888@gmail.com>
2017-04-25 20:17:21 +0800

    Initial commit

---
+++ app/src/main/java/com/seven/intentdemo/TagerActivity.java
@@ -0,0 +1,13 @@
+package com.seven.intentdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TagerActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tager);
+  }
+}
